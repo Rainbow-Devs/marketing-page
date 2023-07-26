69d791845799bb09464c4ca906dc636dc4e1f9e0
K.M. Jones-Eastland <kmje405@gmail.com>
2023-07-26 15:05:58 -0500

    HomeBody component creation

---
+++ src/components/HomeBody/HomeBody.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./HomeBody.scss";
+
+const HomeBody = () => {
+  return (
+    <div className="homepage-body-container">
+      <div className="about-section">
+        <p>hello world</p>
+        {/* Content for the About section goes here */}
+        {/* You can add text, images, or any other components */}
+      </div>
+      <div className="projects-section">
+        {/* Content for the Projects section goes here */}
+        {/* You can add text, images, or any other components */}
+      </div>
+      <div className="join-us-section">
+        {/* Content for the Join Us section goes here */}
+        {/* You can add text, images, or any other components */}
+      </div>
+    </div>
+  );
+};
+
+export default HomeBody;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,16 @@
-// pages/index.tsx
 import * as React from "react";
 import { useEffect } from "react";
 import "../pages/index.scss"; // Import the global CSS file
 import Navbar from "../components/Navbar/Navbar";
 import HeroSection from "../components/HeroSection/HeroSection";
+import HomeBody from "../components/Homebody/HomeBody";
 
 const IndexPage: React.FC = () => {
-  useEffect(() => {
-    // Toggle debug mode by setting this variable to true or false
-    const debugMode = true;
-    if (debugMode) {
-      document.documentElement.classList.add("debug");
-    } else {
-      document.documentElement.classList.remove("debug");
-    }
-  }, []);
-
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <HomeBody />
     </main>
   );
 };

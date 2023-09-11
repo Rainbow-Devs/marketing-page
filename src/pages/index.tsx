a38f18c32a95542f8118005e566f58c83ab60a29
K.M. Jones-Eastland <kmje405@gmail.com>
2023-09-11 11:04:38 -0500

    nav-bar-readibility-updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,22 @@
 import * as React from "react";
 import "../pages/index.scss"; // Import the global CSS file
+
 import Navbar from "../components/Navbar/Navbar";
 import HeroSection from "../components/HeroSection/HeroSection";
 import HomeBody from "../components/HomeBody/HomeBody";
-import Footer from "../components/Footer/Footer"; // Replace "path/to/Footer" with the actual path to your Footer component
-
+import Footer from "../components/Footer/Footer"; 
 const IndexPage: React.FC = () => {
   return (
-    <main>
-      <Navbar />
-      <HeroSection />
-      <HomeBody />
-      <Footer />
-    </main>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <HeroSection />
+        <HomeBody />
+        <Footer />
+      </main>
+    </>
   );
 };
 
